9c1422650abdffccb31041b7b0486c316afdba54
RemiPoly <remiliance@yahoo.fr>
2023-05-17 09:52:07 +0200

    frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+// frontend/src/index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
